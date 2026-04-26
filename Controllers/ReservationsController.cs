8e34243980ff33f7d0e020a74a174faa398762c7
Andriy23928392 <polupanovandrej53@gmail.com>
2026-04-26 20:01:12 +0300

    clean

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -5,7 +5,7 @@ using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Controllers
 {
-    [Route("api/[controller]")] // Автоматично створює маршрут /api/reservations
+    [Route("api/[controller]")] 
     [ApiController]
     public class ReservationsController : ControllerBase
     {
@@ -16,26 +16,21 @@ namespace RestaurantAPI.Controllers
             _context = context;
         }
 
-        // 1. ОТРИМАТИ ВСІ БРОНЮВАННЯ (щоб ти міг перевірити, чи вони зберігаються)
-        // Запит: GET /api/reservations
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
         {
             return await _context.Reservations.ToListAsync();
         }
         [HttpPost]
-        // 2. СТВОРИТИ НОВЕ БРОНЮВАННЯ (Сюди буде стукати фронтенд)
-        // Запит: POST /api/reservations
+
         [HttpPost]
         public async Task<ActionResult<Reservation>> CreateReservation(Reservation reservation)
         {
-            // Додаємо нову броню в базу
             _context.Reservations.Add(reservation);
 
-            // Зберігаємо зміни
             await _context.SaveChangesAsync();
 
-            // Повертаємо успішну відповідь 200 OK з повідомленням
             return Ok(new { message = "Столик успішно заброньовано!", data = reservation });
         }
     }
